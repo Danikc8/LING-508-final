1c070aad151381f03b1c0889cb60f92736591c40
Dani Cannella <dcannell@usc.edu>
2025-08-18 00:13:01 -0700

    Added Flask API

--- app.py
+++ app.py
@@ -25,11 +25,49 @@ app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app, resources={r"/parse": {"origins": "http://localhost:port"}})
 
-services = Services()
-
 
 @app.route('/')
 def doc() -> str:
     app.logger.info("doc - Got request")
     with open("app/doc.html", "r") as f:
         return f.read()
+
+
+@app.route("/pronounce", methods=["POST"])
+@cross_origin(origin='localhost', headers=['Content-Type', 'Authorization'])
+def pronounce():
+    data = request.get_json()
+    app.logger.info(f"/pronounce - Got request: {data}")
+    services = Services(data.get('word'))
+    filepath = services.pronounce()
+
+    os.system(f'mpg123 "{filepath}"')
+    os.remove(filepath)
+
+    return jsonify({"msg": "success"})
+
+
+@app.route("/search", methods=["POST"])
+@cross_origin(origin='localhost', headers=['Content-Type', 'Authorization'])
+def search():
+    data = request.get_json()
+    app.logger.info(f"/search - Got request: {data}")
+    services = Services(data.get('word'))
+    character = services.fetch()
+    ret = [{
+        "form": character.character,
+        "pos": e.pos.name if hasattr(e.pos, "name") else str(e.pos),
+        "romanization": e.romanization,
+        "eng_tran": e.eng_tran,
+        "phon_comp": {
+            "onset": e.phon_comp.onset if e.phon_comp else None,
+            "nucleus": e.phon_comp.nucleus if e.phon_comp else None,
+            "coda": e.phon_comp.coda if e.phon_comp else None,
+            "tone": e.phon_comp.tone if e.phon_comp else None
+        } if e.phon_comp else None
+    } for e in character.lex_entries][:1]
+
+    return jsonify(ret)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
